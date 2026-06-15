02a655641d579fae09a9e2ade4d31529701006c8
Siddharth Shukla <siddharthshukla840@gmail.com>
2026-06-15 21:31:38 +0000

    Sync LeetCode submission - Reverse String (python3)

---
+++ problems/reverse_string/solution.py
@@ -0,0 +1,16 @@
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        low = 0
+        high = len(s) - 1
+        
+        while low < high:
+            tmp = s[low]
+            s[low] = s[high]
+            s[high] = tmp
+            
+            low += 1
+            high -= 1
+        
