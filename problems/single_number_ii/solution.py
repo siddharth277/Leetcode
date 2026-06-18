45f109e7ad1ba3c35eabe1bd3ebb671ee99f02ed
Siddharth Shukla <siddharthshukla840@gmail.com>
2026-06-18 16:39:58 +0000

    Sync LeetCode submission - Single Number II (python3)

---
+++ problems/single_number_ii/solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        i=0
+        nums.sort()
+        while(i<len(nums)-1):
+           
+            if nums[i]==nums[i+1] : 
+                i+=3
+            else :
+                return nums[i]
+        return nums[len(nums)-1]
