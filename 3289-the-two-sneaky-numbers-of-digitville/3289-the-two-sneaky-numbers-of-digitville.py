12ecf35b9ecc03ee1347bc469864df2f3538376a
Siddharth Shukla <siddharthshukla840@gmail.com>
2026-07-11 17:13:25 +0530

    Time: 0 ms (100%), Space: 19.3 MB (21.73%) - LeetHub

---
+++ 3289-the-two-sneaky-numbers-of-digitville/3289-the-two-sneaky-numbers-of-digitville.py
@@ -0,0 +1,12 @@
+class Solution:
+    def getSneakyNumbers(self, nums: List[int]) -> List[int]:
+        arr=[]
+        nums.sort()
+        for i in range(len(nums)-1):
+            if nums[i]==nums[i+1] :
+                arr.append(nums[i])
+        return arr
+
+
+
+        
