571f16c20a3786af3e83142265ff1eaab7d7e425
Siddharth Shukla <siddharthshukla840@gmail.com>
2026-07-11 22:57:32 +0530

    Time: 0 ms (100%), Space: 19.2 MB (86.81%) - LeetHub

---
+++ 2729-check-if-the-number-is-fascinating/2729-check-if-the-number-is-fascinating.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isFascinating(self, n: int) -> bool:
+        def ronaldo(num):
+            if sorted(list(num))==["1","2","3","4","5","6","7","8","9"]:
+                return(True)
+            else:
+                return False
+        n=str(n)+str(n*2)+str(n*3)
+        return ronaldo(n)
